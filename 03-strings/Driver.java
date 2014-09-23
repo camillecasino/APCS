8667790f5224716180cd843105476a829d359d0d
camillecasino <camille.casino@gmail.com>
2014-09-23 08:29:17 -0400

    strings

---
+++ 03-strings/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+    public static void main(String[] args){
+	String s="camille casino";
+        int space = s.indexOf(" ");
+	String firstname = s.substring(0, space);
+	String lastname = s.substring(space);
+
+
+        }
+}
