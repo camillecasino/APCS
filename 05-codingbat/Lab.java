5cf3bec3534e925e1a606c685e5761e21865f253
camillecasino <camille.casino@gmail.com>
2014-09-24 08:41:10 -0400

    commited

--- 05-codingbat/Lab.java
+++ 05-codingbat/Lab.java
@@ -1,28 +1,48 @@
+public class StringStuff {
+
+    public String nonStart (String a, String b) {
+	return a.substring(1) + b.substring (1);
+    }
+
+    public int diff21(int n) {
+	if (n < 21) {
+	    return Math.abs(n - 21);
+	}
+	return Math.abs((21 - n) * 2);
+    }
+
+
+
+
+
+//lab problems; worked with kevin mejia
 
 //working
-public boolean nearHundred(int n) {
-    return (Math.abs(n - 100) <= 10)) || (Math.abs(n - 200) <= 10);
+    public boolean nearHundred(int n) {
+        return (Math.abs(n - 100) <= 10)) || (Math.abs(n - 200) <= 10);
 }
 
 //working
-public boolean mixStart(String str) {
-	if (str.length() < 3)
+    public boolean mixStart(String str) {
+	 if (str.length() < 3)
 	    return false;
 	String str2 = str.substring(1, 3);
 	return(str2.equals("ix"));
 }
 
-public int teaParty(int tea, int candy) {
-    if ((tea>=(2 * candy) || candy>=(2 *tea)) && ((tea >= 5)  && (candy >=5)))  {
-	return 2  ;
+    public int teaParty(int tea, int candy) {
+        if ((tea>=(2 * candy) || candy>=(2 *tea)) && ((tea >= 5)  && (candy >=5)))  {
+	    return 2  ;
 	}
-    if ((tea >= 5)  && (candy >=5)) {
-	return 1;
-	} else { 
+        if ((tea >= 5)  && (candy >=5)) {
+	    return 1;
+	}   else { 
 	    return 0;
 	}
      
     }
-public boolean lastDigit(int a, int b, int c) {
-    return (a % 10 == b % 10) || (a % 10 == c % 10) || (b % 10 == c % 10);
+    public boolean lastDigit(int a, int b, int c) {
+        return (a % 10 == b % 10) || (a % 10 == c % 10) || (b % 10 == c % 10);
     }
+
+}
