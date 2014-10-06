8655fbb84f44b75bac0e6769a14675883ddb6bed
camillecasino <camille.casino@gmail.com>
2014-10-06 08:06:43 -0400

    codes.java

---
+++ 08-more/Frame.java
@@ -0,0 +1,12 @@
+public class framestuff {
+    
+    public String line(String s, int c) {
+	int a = 0;
+	String n
+	while (a < c) {
+	    
+
+    public String frame(int r, int c) {
+	int n = r - 2;
+	
+	    
