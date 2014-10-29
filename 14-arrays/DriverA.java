c2e7c86c834daec66a387290f305ad28ec87e019
camillecasino <camille.casino@gmail.com>
2014-10-29 08:16:04 -0400

    hw14 driver

---
+++ 14-arrays/DriverA.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class DriverA {
+    public static void main(String[] args) {
+	public int find(int value) {
+	    for (int i = 0; i < a.length; i++) {
+		if (a[i] == value) {
+		    return i;
+		}
+		return -1;
+	    }
+	}
+    }
+}
