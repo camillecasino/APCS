a7267c7ee22727fee13711fe92f9f10ec6c12980
camillecasino <camille.casino@gmail.com>
2014-11-07 08:15:59 -0500

    Sarray.java

---
+++ Sarray.java
@@ -0,0 +1,19 @@
+public class Sarray {
+    private int[] data; //should be object[]
+    private int[] last;
+
+    public Sarray() {
+	//start array @ size 10
+    }
+
+    add(int i); //add to end
+    add(int index, int i); //add at location index shift everything else down
+    get(int index);
+    set(int index, int i);
+    size();
+    remove(int index) //must shift things over
+
+
+
+	
+}
