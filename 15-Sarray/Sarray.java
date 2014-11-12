c9fa4fc434536d90ec6fa8745f442af6dd10322e
Elga Casino <Elga@paulstenosimac3.home>
2014-11-12 06:26:18 -0500

    Added errors

--- 15-Sarray/Sarray.java
+++ 15-Sarray/Sarray.java
@@ -7,21 +7,55 @@ public class Sarray {
 
     public Sarray(int i) {
         data = new int[i];
+	last = 0;
 	r = new Random();
 	for (int n = 0; n < data.length; n++) {
 	    data[n] = r.nextInt(10);
 	}
     }
+
+    public String toString() {
+	String f = "";
+	for (int i = 0; i < data.length; i++) {
+	    f = f + data[i] + ", ";
+	    if (i == data.length - 1) {
+		f = f + data[i];
+	    }
+	}
+	return f;
+    }
     
+     public void lengthen() {
+	if (last >= data.length) {
+	    int[] ans = new int[data.length + 10];
+	    for (int i = 0; i < data.length; i++){
+		ans[i] = data[i];
+	    }
+	    data = ans;
+	}
+    }
+
     public boolean add(int i){
-	
-        // adds an item to the end of the list, grow if needed
-        // returns true
+        lengthen();
+	i = data[data.length - 1];
+	last = last + 1;
+	return true;
     }
 
     public void  add(int index, int i){
-        // adds item i  at index, shifting everything down as needed.
-        // also grows as needed 
+        if (index <= data.length - 1) {
+	    for (int n = last; n > 0; n--) {
+		if (n > index) {
+		    data[n] = data[n - 1];
+		} else if (i == index) {
+		    data[i] = n;
+		    last = last + 1;
+		}
+	    }
+	} else {
+	    throw new ArrayIndexOutOfBoundsException();
+	}
+
     }
     
     public int size() {
@@ -34,22 +68,40 @@ public class Sarray {
 	    }
 	}
 	return c;
-        // returns the number of items in the list (not the array size)
     }
-    /*
+    
     public int get(int index) {
-        // returns the item at location index of the lsit
+        if (index <= data.length - 1) {
+	    return data[index];
+	} else {
+	    throw new ArrayIndexOutOfBoundsException();
+	}
     }
 
     public int set(int index, int i){
-        // sets the item at location index to value i
-        // returns the old value. 
+        if (index <= data.length - 1) {
+	    int p = data[index];
+	    data[index] = i;
+	    return p;
+	} else {
+	    throw new ArrayIndexOutOfBoundsException();
+	}
     }
 
     public int remove(int index){
-        // removes the item at index i
-        // returns the old value
+	if (index <= data.length - 1) {
+	    int p = data[index];
+	    int[] f = new int[data.length - 1];
+	    for (int i = 0; i < last; i++) {
+		if (i != index) {
+		    f[i] = data[i];
+		}
+	    }
+	    data = f;
+	    last = last - 1;
+	    return p;
+	} else {
+	    throw new ArrayIndexOutOfBoundsException();
+	}
     }
-}
-    */
 }
