4c1802e501c7d4d11cfe9379fb508c1dc3c1a623
Elga Casino <Elga@paulstenosimac3.home>
2014-11-13 05:33:47 -0500

    Wrong file committed, fixing it

--- 16-ArrayLists/Driver.java
+++ 16-ArrayLists/Driver.java
@@ -2,20 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Driver {
     public static void main(String[] args) {
-	ArrayList<String> al;
-	al = new ArrayList<String>();
-	System.out.println(al);
-	al.add("hello");
-	System.out.println(al);
-	
-	al.add("world");
-	System.out.println(al);
-	
-	al.add("33");
-	System.out.println(al);
-	System.out.println(al.size());
-	System.out.println(al.get(1));
-	
+
 	ArrayList<Integer> ai;
 	ai=new ArrayList<Integer>();
 	Random r = new Random();
@@ -24,6 +11,18 @@ public class Driver {
 	}
 	
 	System.out.println(ai);
+    
+	for (int n = 0; n < ai.size() - 1; n++) {
+	    int p = ai.get(n);
+	    for (int m = n; m < ai.size(); m++) {
+		if (ai.get(m + 1) == p) {
+		    ai.remove(m + 1);
+		}
+	    }
+	}
+	System.out.println(ai);
+
+
     }
     
 }
