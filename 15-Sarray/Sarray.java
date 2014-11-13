13641d01284d795ce6c50a7b5409399774731b79
camillecasino <camille.casino@gmail.com>
2014-11-13 08:08:48 -0500

    committing

--- 15-Sarray/Sarray.java
+++ 15-Sarray/Sarray.java
@@ -11,29 +11,44 @@ public class Sarray {
 	for (int n = 0; n < data.length; n++) {
 	    data[n] = r.nextInt(10);
 	}
+	last = 0;
+    }
+
+    public void lengthen() {
+	if (last >= data.length) {
+	    int[] ans = new int[data.length + 10];
+	    for (int i = 0; i < data.length; i++) {
+		ans[i] = data[i];
+	    }
+	    data = ans;
+	}
     }
     
     public boolean add(int i){
-	
+	lengthen();
+	data[data.length - 1] = i;
+	last = last + 1;
+	return true;
         // adds an item to the end of the list, grow if needed
         // returns true
     }
 
     public void  add(int index, int i){
+	lengthen();
+	for (int n = lastIndex; n > 0; n--) {
+	    if (n > index) {
+		data[n] = data[n - 1];
+	    } else if (i == index) {
+		data[n] = i;
+		last = last + 1;
         // adds item i  at index, shifting everything down as needed.
-        // also grows as needed 
+        // also grows as needed
+	    }
+	}
     }
     
     public int size() {
-	int c = 0;
-	for (int i = 0; i < data.length; i++) {
-	    if (data[i] < 0 || data[i] > 0 || data[i] == 0) {
-		c++;
-	    } else {
-		c = c;
-	    }
-	}
-	return c;
+	return last;
         // returns the number of items in the list (not the array size)
     }
     /*
