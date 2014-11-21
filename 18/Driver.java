4f1d9b012096b53f626da8b91ad40a06b5c133f3
camillecasino <camille.casino@gmail.com>
2014-11-21 08:39:30 -0500

    unfinished

--- 18/Driver.java
+++ 18/Driver.java
@@ -1,8 +1,10 @@
 public class Driver {
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
-	w.addWordH("hello", 3, 14);
-	w.addWordH("look", 3, 8);
+	w.addWordHF("hello", 3, 1);
+	w.addWordHB("look", 9, 8);
+	w.addWordVD("camille", 2, 7);
+	w.addWordDUR("apcompsci", 14, 19);
 	System.out.println(w);
     }
 }
