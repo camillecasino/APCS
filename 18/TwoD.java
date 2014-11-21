af105ac6c9515669e4d9fb670bed7901b88fecac
camillecasino <camille.casino@gmail.com>
2014-11-21 08:01:11 -0500

    TwoD.java

---
+++ 18/TwoD.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class TwoD {
+    public static void main(String[] args) {
+	int[][] a;
+	a = new int[5][3];
+
+	for (int i = 0; i < a.length; i++) {
+	    System.out.println("row: "+ i + " has length: " + a[i].length);
+	    for (int j = 0; j < a[i].length; j++) {
+		System.out.print(a[i][j]+ ",");
+	    }
+	    System.out.println();
+	}
+    }
+}
