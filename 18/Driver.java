cdd69c3f17d87684661f9b7973a3ce3ae60691bc
Elga Casino <Elga@paulstenosimac3.home>
2014-11-24 00:28:25 -0500

    added tests

--- 18/Driver.java
+++ 18/Driver.java
@@ -1,9 +1,13 @@
 public class Driver {
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
-        //w.addWordVD("camille", 5, 9);
-	//w.addWordW("mama", 11, 16);
-	//w.addWordDUNE("papa", 9, 24);
+	w.addWord("camille");
+	w.addWord("stuyvesant");
+	w.addWord("apcompsci");
+	w.addWord("zamansky");
+	w.addWord("helloworld");
+	System.out.println(w);
+	w.fillBoard(21, 45);
 	System.out.println(w);
     }
 }
