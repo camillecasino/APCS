fb6cfcc75bca4442dd1addda5efe28fae8af7bda
camillecasino <camille.casino@gmail.com>
2014-11-24 08:40:38 -0500

    added some more tests

--- 18/Driver.java
+++ 18/Driver.java
@@ -1,13 +1,21 @@
 public class Driver {
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
-	w.addWord("camille");
-	w.addWord("stuyvesant");
-	w.addWord("apcompsci");
-	w.addWord("zamansky");
-	w.addWord("helloworld");
+	w.addWord("CAMILLE");
+	w.addWord("STUYVESANT");
+	w.addWord("APCOMPSCI");
+	w.addWord("ZAMANSKY");
+	w.addWord("HELLOWORLD");
+	w.addWord("ULTIMATE");
+	w.addWord("FRISBEE");
+	w.addWord("COMPUTER");
+	w.addWord("HOORAY");
+	w.addWord("VOLLEYBALL");
+	w.addWord("OUTSIDE");
+	w.addWord("MIDDLE");
+	w.addWord("WEAKSIDE");
 	System.out.println(w);
-	w.fillBoard(21, 45);
+	w.fillBoard();
 	System.out.println(w);
     }
 }
