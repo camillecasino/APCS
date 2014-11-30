b6a6f4b277c710e09aeeab8e0208c5def6017075
Elga Casino <Elga@paulstenosimac3.home>
2014-11-30 09:23:04 -0500

    Driver

---
+++ 18-extra/Driver.java
@@ -0,0 +1,21 @@
+public class Driver {
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch();
+	//	w.addWord("CAMILLE");
+	//	w.addWord("STUYVESANT");
+	//	w.addWord("APCOMPSCI");
+	//	w.addWord("ZAMANSKY");
+	//	w.addWord("HELLOWORLD");
+	//	w.addWord("ULTIMATE");
+	//	w.addWord("FRISBEE");
+	//	w.addWord("COMPUTER");
+	//	w.addWord("HOORAY");
+	//	w.addWord("VOLLEYBALL");
+	//	w.addWord("OUTSIDE");
+	//	w.addWord("MIDDLE");
+	//	w.addWord("WEAKSIDE");
+	System.out.println(w);
+	//	w.fillBoard();
+	w.buildPuzzle(10);
+	System.out.println(w);
+    }
