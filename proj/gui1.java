ee92c43151f78c5fb0a03113284011e9f24452ce
camillecasino <camille.casino@gmail.com>
2014-12-17 08:40:44 -0500

    Sticky and gui

--- proj/gui1.java
+++ proj/gui1.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class gui1 extends JFrame {
+public class gui1 extends JFrame implements ActionListener {
 
     private Container pane;
     private JButton b1;
@@ -9,6 +10,31 @@ public class gui1 extends JFrame {
     private JLabel l;
     private JTextArea t;
     private JPanel canvas;
+
+    private class Canvas extends JPanel {
+	public void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+	    g.setColor(Color.red);
+	    g.fillOval(30,30,30,30);
+	}
+    }
+    /*
+    private class Key implements KeyListener {
+	public void KeyPressed(KeyEvent e) {}
+	public void KeyReleased(KeyEvent e) {}
+	public void KeyTyped(KeyEvent e) {}
+    }
+    */
+
+    public void actionPerformed(ActionEvent e) {
+	if (e.getSource() == b1) {
+	    System.out.println("You clicked click me");
+	    System.out.println("By the way, the text box has " + t.getText());
+	} else if (e.getSource() == b2) {
+	    System.out.println("Shutting down");
+	    System.exit(0);
+	}
+    }
     
     public gui1() {
 	setTitle("My First Gui");
@@ -22,12 +48,15 @@ public class gui1 extends JFrame {
 	pane.add(l);
 	b1 = new JButton("Button");
 	pane.add(b1);
+	b1.addActionListener(this);
 	b2 = new JButton("exit");
 	pane.add(b2);
+	b2.addActionListener(this);
 	t = new JTextArea("  ");
 	t.setColumns(30);
 	t.setRows(20);
 	t.setBorder(BorderFactory.createLineBorder(Color.red,2));
+	//t.addKeyListener(Key);
 	pane.add(t);
 
 	canvas = new JPanel();
